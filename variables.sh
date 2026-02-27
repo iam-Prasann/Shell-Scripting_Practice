3cae0f24e15eae4fc9ec54f48729e54032fb74e8
root <root@ip-172-31-14-149.ec2.internal>
2026-02-27 07:43:38 +0000

    This is my second script

---
+++ variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+########################################
+# Author : Prasann
+# Date : 27-02-2026
+#
+# This script is about variables and is my second script
+# ######################################
+
+ Name="Prasann"
+
+ echo $Name
