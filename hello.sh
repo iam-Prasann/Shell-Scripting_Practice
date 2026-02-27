398d8e27001b55ef2a0f422eaba28d444ce8bd9b
root <root@ip-172-31-14-149.ec2.internal>
2026-02-27 07:08:56 +0000

    This is my first shell script

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+################################################
+# Author : Prasann
+# Date : 27-02-2026
+#
+# This is my first shell script
+# ##############################################
+
+ echo "hello world"
