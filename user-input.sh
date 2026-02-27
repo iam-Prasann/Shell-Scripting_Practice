edeaee63dff85c5070c6d515a65ac29134dacf84
root <root@ip-172-31-14-149.ec2.internal>
2026-02-27 12:24:32 +0000

    This is my 3rd script

---
+++ user-input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+########################################
+# Autor : Prasann Parab
+# Date : 27-02-2026
+# This script takes user input
+########################################
+
+read -p  "Enter username:" username
+
+echo "You enterd a user $username"
+
+
